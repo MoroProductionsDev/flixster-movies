180592b64b9694583528b0aa68fc27e5a862d401
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-18 18:50:07 -0800

    Initial commit Added a new View Controller (MoviesViewController) Linked the MoviesViewController in the identity inspector custom class.

---
+++ flixster/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by RAUL RIVERO RUBIO on 2/18/20.
+//  Copyright © 2020 codepath-cit238b-spring2020. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
