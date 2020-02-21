9d6893d0255a567c8a76b3136000c3b3a2d065e8
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-20 18:23:37 -0800

    - added the API base url and created the movie "poster_path"
    ! Needs to install the cocopods from the terminal.

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -64,6 +64,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let title = movie["title"] as! String
         let synopsis = movie["overview"] as! String
         
+        // API image base url for (jpg)
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        // add the movie poster to the path
+        let posterPath = movie["poster_path"] as! String
+        // combine the api baseUrl with movies json posterPath
+        let posterUrl = URL(string : baseUrl + posterPath)
+        
         cell.titleLabel!.text = title
         cell.synopsisLabel!.text = synopsis
         
