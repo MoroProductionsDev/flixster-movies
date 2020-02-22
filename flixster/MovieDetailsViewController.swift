e3e90f5d8f17a7784e4f77dd75554acd8d424cdc
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-21 17:37:37 -0800

    - Added another view controller. - Create a Cocoa class "MovieDetailsViewController" will hold the necessary information. - Linked the screen with the class.

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by RAUL RIVERO RUBIO on 2/21/20.
+//  Copyright © 2020 codepath-cit238b-spring2020. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
