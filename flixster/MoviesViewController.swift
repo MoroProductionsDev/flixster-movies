b9ef71ef7fa8a3c3d39d667f422263063b92adb2
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-21 18:15:05 -0800

    - Uncommented the prepare()  that will prepare the data to be display in that view. - Found the selective movie and Pass the selective movie to the details view controller - Deselect the screen

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -40,7 +40,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 // At this point "self.movies" has been fetch so reload all the function and re executing them
                 self.tableView.reloadData()
                 
-                print(dataDictionary);
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
                 // TODO: Reload your table view data
@@ -81,14 +80,24 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        // Find the selective movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selective movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        // Deselect the screen
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
 }
