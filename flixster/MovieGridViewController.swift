378dab07d10d2dbb67189f8fd7a2843803a8c98c
RAUL RIVERO RUBIO <5000928902@mwccpod10-06.local>
2020-02-24 14:36:29 -0800

    * Previous problem fixed. [SIGABRT Error]. MovieGridCell was not properly identify. - Configure the layout

--- flixster/MovieGridViewController.swift
+++ flixster/MovieGridViewController.swift
@@ -16,8 +16,19 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 3) / 3
+        let height = width * 1.5
+        layout.itemSize = CGSize(width: width, height: height)
+
+
+        
         collectionView.delegate = self
         collectionView.dataSource = self
+        
         
         // Do any additional setup after loading the view.
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
